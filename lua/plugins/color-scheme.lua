772abd99a63f0a89e41a111de41cdbdb78ec3f0e
elia-b <elia.boscaini@gmail.com>
2025-03-07 18:26:57 +0100

    Fix color scheme

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -5,8 +5,7 @@ return {
 	priority = 1000, -- make sure to load this before all the other start plugins
 	-- Optional; default configuration will be used if setup isn't called.
 	config = function()
-		require("everforest").setup({
-			vim.cmd([[colorscheme everforest]]),
-		})
+		require("everforest").setup()
+		vim.cmd.colorscheme("everforest")
 	end,
 }
