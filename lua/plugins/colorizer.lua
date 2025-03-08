5b3b99bfee2b3e143f08bce86eb229f095be147e
elia-b <elia.boscaini@gmail.com>
2025-03-08 11:31:41 +0100

    Fix color scheme to everforest hard

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,7 +1,13 @@
 return {
 	"catgoose/nvim-colorizer.lua",
 	event = "BufReadPre",
+	config = function()
+		require("colorizer").setup({
+			user_default_options = {
+				names = false, -- "Name" codes like Blue or red.
+			},
+		})
+	end,
 	opts = { -- set to setup table
-		names = false,
 	},
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,27 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	opts = {
-		options = {
-			path = 1,
-			icons_enabled = true,
-			component_separators = "",
-			section_separators = "",
-		},
-	},
+
+	config = function()
+		local everforest_hard = require("lualine.themes.everforest")
+
+		-- Change the background of lualine_c section for normal mode
+		everforest_hard.normal.c.bg = "#272e33"
+
+		require("lualine").setup({
+			options = {
+				path = 1,
+				component_separators = "",
+				section_separators = "",
+				theme = everforest_hard,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" }, -- Remove 'encoding' if it was here
+				lualine_x = { "filetype" }, -- Remove 'encoding' from here
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
