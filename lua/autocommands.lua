6b59572b9e54728c763a3762dd4e911f444091c3
elia-b <elia.boscaini@gmail.com>
2025-10-26 01:28:24 +0200

    Reopen file on last edited line

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -5,3 +5,20 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+-- Remember cursor position when reopening files
+vim.api.nvim_create_augroup("remember_cursor_position", { clear = true })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	group = "remember_cursor_position",
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local line_count = vim.api.nvim_buf_line_count(0)
+		local line = mark[1]
+
+		if line > 1 and line <= line_count then
+			vim.cmd('normal! g`"')
+			vim.cmd("normal! zz")
+		end
+	end,
+})
