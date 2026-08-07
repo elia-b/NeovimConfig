4850ba0a0aff93622a00180be11a0a08552d2f1b
elia-b <elia.boscaini@gmail.com>
2026-08-07 14:48:25 +0200

    ✨ Update treesitter, and add vimtex

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -49,7 +49,15 @@ vim.diagnostic.config({
 	virtual_lines = true, -- Text shows up underneath the line, with virtual lines
 
 	-- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
-	jump = { float = true },
+	jump = {
+		on_jump = function(_, bufnr)
+			vim.diagnostic.open_float({
+				bufnr = bufnr,
+				scope = "cursor",
+				focus = false,
+			})
+		end,
+	},
 })
 
 vim.opt.fillchars:append({ eob = " " })
